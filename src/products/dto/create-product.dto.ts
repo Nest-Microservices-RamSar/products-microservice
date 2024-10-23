1bee9b3913b5632a71a5a3548fee820a6d73bbc6
ramiro <ramiro.sarasola@gmail.com>
2024-10-23 20:11:25 -0300

    add env variables with validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,13 @@
-export class CreateProductDto {}
+import { Type } from 'class-transformer';
+import { IsNumber, IsPositive, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  public name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  @Min(0)
+  @Type(() => Number)
+  public price: string;
+}
